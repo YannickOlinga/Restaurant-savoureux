42cb0e72dba7f1ea192f6fbe155e8efd2fbdf670
yannickolinga <yannickolinga213@gmail.com>
2026-04-16 19:18:17 +0000

    Hello

--- resto/resto/wsgi.py
+++ resto/resto/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resto.resto.settings')
 
 application = get_wsgi_application()
